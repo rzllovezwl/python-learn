37aa987ce545a89dd3a3d05ff134b97d920fd037
rzllovezwl <584772541@qq.com>
2018-09-20 00:30:11 +0800

    类和对象的实例属性的变量地址是否指向一个地址

--- 01.py
+++ 01.py
@@ -31,3 +31,59 @@ print(lili.name)
 print(lili.age)
 # 注意成员函数的调用没有传入参数
 lili.DoHomework()
+print('-' * 60)
+
+'''
+例子：类和对象的成员分析
+'''
+class A():
+    name = 'wenli'
+    age = 18
+
+    def say(self):
+        self.name = 'aaaa'
+        self.age = 200
+# 此案例说明：
+#   类实例的属性和其对象的实例的属性在不对对象的实例属性赋值的前提下，
+#   指向同一个变量
+# 此时，A称为类实例
+print(A.name)
+print(A.age)
+
+print('*' * 20)
+
+# id可以鉴别一个变量是否和另一个变量是同一个变量
+print(id(A.name))
+print(id(A.age))
+
+print('*' * 20)
+a = A()
+
+print(a.name)
+print(a.age)
+print(id(a.name))
+print(id(a.age))
+
+print('-' * 70)
+'''
+给对象的实例赋值的情况下，属性指向不同变量
+'''
+# 此时，A称为类实例
+print(A.name)
+print(A.age)
+
+print('*' * 20)
+
+# id可以鉴别一个变量是否和另一个变量是同一个变量
+print(id(A.name))
+print(id(A.age))
+
+print('*' * 20)
+a = A()
+a.name = 'zhenglin'
+a.age = 19
+
+print(a.name)
+print(a.age)
+print(id(a.name))
+print(id(a.age))
