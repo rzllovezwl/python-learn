6134eeea0d3f181d1ee318d56bc951551ec6460e
rzllovezwl <584772541@qq.com>
2018-09-21 00:45:10 +0800

    关于self的用法以及封装的私有

--- 01.py
+++ 01.py
@@ -1,5 +1,5 @@
 '''
-定义一个学生类，用啦形容学生
+定义一个学生类，用来形容学生
 '''
 # 定义一个空的类
 class Student():
@@ -31,7 +31,7 @@ print(lili.name)
 print(lili.age)
 # 注意成员函数的调用没有传入参数
 lili.DoHomework()
-print('-' * 60)
+print('-' * 70)
 
 '''
 例子：类和对象的成员分析
@@ -80,10 +80,101 @@ print(id(A.age))
 
 print('*' * 20)
 a = A()
+# 查看A内所有的属性
+print(A.__dict__)
+print(a.__dict__)
 a.name = 'zhenglin'
 a.age = 19
+print(a.__dict__)
 
 print(a.name)
 print(a.age)
 print(id(a.name))
 print(id(a.age))
+
+print('-' * 70)
+
+'''
+关于self
+'''
+class Student():
+    name = 'wenli'
+    age = 18
+
+    def say(self):
+        self.name = 'aaaa'
+        self.age = 200
+        print('my name is {0}'.format(self.name))
+        print('my age is {0}'.format(self.age))
+renzhenglin = Student()
+renzhenglin.say()
+
+print('-' * 70)
+
+class Teacher():
+    name = 'wenli'
+    age = 19
+
+    def say(self):
+        self.name = 'zhenglin'
+        self.age = 20
+        print('my name is {0}'.format(self.name))
+        print('my age is {0}'.format(__class__.age)) #访问类的成员变量用 __class_
+    def sayAgain():
+        print(__class__.name) #访问类的成员变量用 __class__
+        print(__class__.age) #访问类的成员变量用 __class_
+        print('hello')
+t = Teacher()
+t.say()
+# 调用绑定类函数使用类名
+Teacher.sayAgain()
+
+print('-' * 70)
+
+class A():
+    name = 'wenli'
+    age = 18
+
+    def __init__(self):
+        self.name = 'aaaa'
+        self.age = 200
+
+    def say(self):
+        print(self.name)
+        print(self.age)
+
+class B():
+    name = 'zhenglin'
+    age = 90
+
+a = A()
+# 此时，系统会默认把a作为第一个参数传入函数
+a.say()
+# 此时，self被a替换
+A.say(a)
+# 同样可以把A作为参数传入
+A.say(A)
+# 此时，传入的是类实例B，因为B具有name和age属性，所以不会报错
+A.say(B)
+# 以上代码，利用了鸭子模型
+
+print('-' * 70)
+
+# 私有变量案例
+
+class Person():
+    # name是公有成员
+    name = 'zhenglin'
+    # __age就是私有成员
+    __age = 18
+p = Person
+# name是公有变量
+print(p.name)
+p._Person__age = 19 # 可以更改变量
+print(p._Person__age)# name mangling技术可以访问到私有变量
+# __age是私有变量
+# 注意报错信息
+#print(p.__age) 访问不到
+
+# name mangling技术
+print(Person.__dict__)
