8460f9975722e1a35530578d3fa90c9157dc82f4
rzllovezwl <584772541@qq.com>
2018-10-08 12:06:03 +0800

    构造函数的使用

--- inherit.py
+++ inherit.py
@@ -75,3 +75,86 @@ class Teacher(Person):
 
 t = Teacher()
 t.work()
+
+print('-' * 70)
+
+# 构造函数的概念
+
+class Dog():
+    # __init__就是构造函数
+    # 每次实例化的时候，第一个被自动调用
+    # 因为主要工作是进行初始化，所以得名
+    def __init__(self):
+        print('i am init in dog')
+# 实例化的时候，括号内的参数需要跟构造函数参数匹配
+kaka = Dog()
+
+print('-' * 70)
+
+# 继承中的构造函数：1
+class Animel():
+    pass
+class PaxingAni(Animel):
+    pass
+class Dog(PaxingAni):
+    # __init__就是构造函数
+    # 每次实例化的时候，第一个被自动调用
+    # 因为主要工作是进行初始化，所以得名
+    def __init__(self):
+        print('i am init in dog')
+# 实例化的时候，自动调用了Dog的构造函数
+kaka = Dog()
+
+print('-' * 70)
+# 继承中的构造函数：2
+
+class Animel():
+    def __init__(self):
+        print('wozuida')
+class PaxingAni(Animel):
+    def __init__(self):
+        print('Paxing Dongwu')
+class Dog(PaxingAni):
+    # __init__就是构造函数
+    # 每次实例化的时候，第一个被自动调用
+    # 因为主要工作是进行初始化，所以得名
+    def __init__(self):
+        print('i am init in dog')
+# 实例化的时候，自动调用了Dog的构造函数
+# 因为找到了构造函数，则不再查找父类的构造函数
+
+kaka = Dog()
+
+print('-' * 70)
+
+# 猫没有构造函数
+class Cat(PaxingAni):
+    pass
+# 此时应该自动调用构造函数，因为Cat没有构造函数，所以查找父类构造函数
+c = Cat()
+
+print('-' * 70)
+
+# 继承中的构造函数：3
+
+class Animel():
+    def __init__(self):
+        print('wozuida')
+class PaxingAni(Animel):
+    def __init__(self,name):
+        print('Paxing Dongwu {0}'.format(name))
+class Dog(PaxingAni):
+    # __init__就是构造函数
+    # 每次实例化的时候，第一个被自动调用
+    # 因为主要工作是进行初始化，所以得名
+    def __init__(self):
+        print('i am init in dog')
+# 实例化Dog时，查找到Dog的构造函数，参数匹配，不报错
+d = Dog()
+
+class Cat(PaxingAni):
+    pass
+# 此时，由于Cat没有构造函数，则向上查找
+# 因为PaxingAni的构造函数需要两个参数，实例化的时候给了一个，报错
+# 此时需要给name参数赋值，变为实参
+c = Cat(name = 'doudou')
