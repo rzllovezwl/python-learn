e948c4e3ce20940e25e2304a5e7691c158c904f3
rzllovezwl <584772541@qq.com>
2018-10-11 00:08:02 +0800

    多态、Mixin设计模式、类的相关内建函数

--- inherit_2.py
+++ inherit_2.py
@@ -43,3 +43,91 @@ class C(A):
     pass
 class D(B, C):
     pass
+
+print('-' * 70)
+
+'''Mixin例子'''
+class Person():
+    name = 'zhangwenli'
+    age = 18
+
+    def eat(self):
+        print('EAT......')
+
+    def drink(self):
+        print('DRINK......')
+
+    def sleep(self):
+        print('SLEEP......')
+
+class Teacher(Person):
+    def work(self):
+        print('WORK......')
+
+class Student(Person):
+    def study(self):
+        print('Study')
+
+class Tutor(Teacher, Student):
+    pass
+
+t = Tutor()
+
+print(Tutor.__mro__)
+print(t.__dict__)
+print(Tutor.__dict__)
+
+print('*' * 70)
+
+class TeacherMixin():
+    def work(self):
+        print('Work')
+
+class StudentMixin():
+    def study(self):
+        print('Study')
+
+class TutorM(Person, TeacherMixin, StudentMixin):
+    pass
+
+tt = TutorM()
+print(TutorM.__mro__)
+print(tt.__dict__)
+print(TutorM.__dict__)
+
+print('-' * 70)
+
+'''issubclass'''
+class A():
+    pass
+class B(A):
+    pass
+class C():
+    pass
+print(issubclass(B, A))
+print(issubclass(C, A))
+print(issubclass(C, object))
+
+print('-' * 70)
+
+'''isinstance'''
+class A():
+    pass
+a = A()
+
+print(isinstance(a, A))
+
+print('-' * 70)
+
+'''hasattr'''
+class A():
+    name = 'NoName'
+a = A()
+print(hasattr(a, 'name'))
+
+print('-' * 70)
+
+'''dir'''
+class A():
+    pass
+print(dir(A))
